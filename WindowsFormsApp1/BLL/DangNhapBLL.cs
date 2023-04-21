5556f16e52fd5061d158f8e4cd7ea568c251bb85
Panda Nguyễn <ndbaochau104@gmail.com>
2023-04-21 07:33:30 +0700

    1

---
+++ WindowsFormsApp1/BLL/DangNhapBLL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.DAL;
+
+namespace WindowsFormsApp1.BLL
+{
+    public class DangNhapBLL
+    {
+        public bool checkLogin(int ten, string mk)
+        {
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(mk);
+            byte[] hasData = new MD5CryptoServiceProvider().ComputeHash(temp);
+            //            var list = hasData.ToList();
+            List<Tai_khoan> list = new List<Tai_khoan>();
+            DangNhapDAL dn = new DangNhapDAL();
+            list = dn.getDsTaiKhoan();
+            foreach (var i in list)
+            {
+                if (i.Ma_TK == ten && i.Mat_khau == mk)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
